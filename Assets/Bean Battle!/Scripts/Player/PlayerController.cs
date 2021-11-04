9385dee1a42e5ce9c0601e800e9a220433291ee5
Kieran <kieranc.inbox@gmail.com>
2021-11-05 09:39:45 +1100

    collider stuff

--- Assets/Bean Battle!/Scripts/Player/PlayerController.cs
+++ Assets/Bean Battle!/Scripts/Player/PlayerController.cs
@@ -46,7 +46,7 @@ namespace Beanbattle.Player
         private void Jump()
         {
             Debug.Log($"isGrounded = {isGrounded} || Time.time - lastTimeGrounded = {Time.time - lastTimeGrounded} (needs to be <=) {rememberGroundedFor} && aditionalJumps = {additionalJumps}");
-            if((isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor) && additionalJumps >= 0)
+            if((isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor) || additionalJumps >= 0)
             {
                 myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
                 additionalJumps--;
@@ -62,8 +62,6 @@ namespace Beanbattle.Player
             float moveBy = x * speed;
             myRigidBody.velocity = new Vector3(moveBy, myRigidBody.velocity.y, 0);
         }
-        
-        
 
         /// <summary> The COOLER jump, jk it just feels better. </summary>
         public void BetterJump()
@@ -97,7 +95,23 @@ namespace Beanbattle.Player
                 isGrounded = true;
             }
         }
-        
+        /// <summary>
+        /// detects if player is touching the ground. if so, resets players maxJumps count.
+        /// </summary>
+        /// <param name="other">object of player</param>
+        private void OnCollisionEnter(Collision other)
+        {
+            if(other.gameObject.CompareTag($"DeathObject"))
+            {
+                PlayerDeath();
+            }
+        }
+
+        private void PlayerDeath()
+        {
+            print("PlayerDeath");
+        }
+
         /// <summary> This lets the player jump even if they fall off the platform </summary>
         private void CheckIfGrounded()
         {
@@ -116,46 +130,5 @@ namespace Beanbattle.Player
                 isGrounded = false;
             }
         }
-        
-    #region Not Working Ground Checks
-
-        /// <summary> Should be checking if the player is touching the ground </summary>
-        private void GroundCheck()
-        {
-            RaycastHit hit;
-            float distance = 1.3f;
-            
-            Vector3 down = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z); 
-            
-            //isGrounded = Physics.Raycast(transform.position, down, out hit, distance,~layerMask);
-            isGrounded = Physics.Raycast(transform.position, down, out hit, distance);
-            // Debug.DrawLine(transform.position, down, Color.cyan,1.3f);
-
-            if(isGrounded)
-            {
-                print("grounded");
-            }
-            else
-            {
-                print("ground not hit and away by - " + hit.distance);
-            }
-        }
-        
-        /// <summary>
-        /// Raycast ground check from the middle not working.
-        /// </summary>
-        /// <returns> Should return true IF you are on the ground. </returns>
-        private bool IsGrounded()
-        {
-            bool raycastHit = Physics.Raycast(transform.position, Vector3.down, 1.1f);
-            Color rayColor;
-
-            rayColor = raycastHit ? Color.green : Color.red;
-            
-            Debug.DrawRay(transform.position, Vector3.down * 1.1f);
-            return raycastHit;
-        }
-    #endregion
-        
     }
 }
