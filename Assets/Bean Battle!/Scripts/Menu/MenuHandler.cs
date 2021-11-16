fb7d4a8a78151ba21e7da9050c0fb021a69322de
Noahsonicx <53206519+Noahsonicx@users.noreply.github.com>
2021-11-16 14:00:27 +1100

    e

---
+++ Assets/Bean Battle!/Scripts/Menu/MenuHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{  
+public void ChangeScene(int sceneIndex)
+{
+SceneManager.LoadScene(sceneIndex);
+]
+}
